79f0e6fb3a2e701e892ae55635c33a269b629c1e
architvohra123 <archit0249.be21@chitkara.edu.in>
2024-06-30 01:49:27 +0530

    user creater controller in route.js

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,27 @@ router.get(`/`, async (req, res) =>{
     res.send(userList);
 })
 
+// to create a user
+router.post('/', async(req, res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        color: req.body.color,
+        passwordHash: req.body.passwordHash,
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+    })
+    user = await user.save()
+
+    if(!user){
+        return res.status(400).send('the user cannot be created')
+    }
+
+    res.send(user)
+})
+
 module.exports =router;
