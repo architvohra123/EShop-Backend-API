207f57da098e057cda25c54437ae55fd01637bc2
architvohra123 <archit0249.be21@chitkara.edu.in>
2024-07-18 21:51:31 +0530

    Ugrade -> Added features to get total sales count of orders and userorders of specific user

--- routes/orders.js
+++ routes/orders.js
@@ -30,21 +30,29 @@ router.get('/:id', async (req, res) =>{
     res.send(order);
 });
 
-router.post('/', async (req, res) => {
-    const orderItemsIds = await Promise.all(req.body.orderItems.map(async (orderItem) => {
+router.post('/', async (req,res)=>{
+    const orderItemsIds = Promise.all(req.body.orderItems.map(async (orderItem) =>{
         let newOrderItem = new OrderItem({
             quantity: orderItem.quantity,
             product: orderItem.product
-        });
+        })
 
         newOrderItem = await newOrderItem.save();
-        return newOrderItem._id;
-    }));
 
-    console.log(orderItemsIds);
+        return newOrderItem._id;
+    }))
+    const orderItemsIdsResolved =  await orderItemsIds;
+
+    const totalPrices = await Promise.all(orderItemsIdsResolved.map(async (orderItemId)=>{
+        const orderItem = await OrderItem.findById(orderItemId).populate('product', 'price');
+        const totalPrice = orderItem.product.price * orderItem.quantity;
+        return totalPrice
+    }))
+
+    const totalPrice = totalPrices.reduce((a,b) => a +b , 0);
 
     let order = new Order({
-        orderItems: orderItemsIds,
+        orderItems: orderItemsIdsResolved,
         shippingAddress1: req.body.shippingAddress1,
         shippingAddress2: req.body.shippingAddress2,
         city: req.body.city,
@@ -52,18 +60,16 @@ router.post('/', async (req, res) => {
         country: req.body.country,
         phone: req.body.phone,
         status: req.body.status,
-        totalPrice: req.body.totalPrice,
-        user: req.body.user
-    });
-
+        totalPrice: totalPrice,
+        user: req.body.user,
+    })
     order = await order.save();
 
-    if (!order) {
-        return res.status(400).send('the order cannot be created');
-    }
+    if(!order)
+    return res.status(400).send('the order cannot be created!')
 
     res.send(order);
-});
+})
 
 // update an order
 router.put('/:id', async (req, res) => {
