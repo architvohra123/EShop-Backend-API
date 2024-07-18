430abf9116de1b014eac53ca10c09991f3aed0df
architvohra123 <archit0249.be21@chitkara.edu.in>
2024-07-18 17:58:41 +0530

    upgrade -> added put and delete api for Order

--- routes/orders.js
+++ routes/orders.js
@@ -65,4 +65,33 @@ router.post('/', async (req, res) => {
     res.send(order);
 });
 
+// update an order
+router.put('/:id', async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+           status: req.body.status
+        },
+        { new: true } // without this line the object sent to client is not the updated one but the older one only so by using this we can send the updated one in res.send(category)
+    )
+    if(!order) {
+        res.status(400).json({message: "the order cannot be created"})
+    } 
+    res.status(200).send(order);
+})
+
+//delete an order
+router.delete('/:id', (req, res) => {           // by id
+    Order.findByIdAndDelete(req.params.id)
+    .then(order => {
+        if(order){
+            return res.status(200).json({success: true, message: 'The order is deleted'})
+        }else{
+            return res.status(404).json({success: false, message: "Order not found"})
+        }
+    })
+    .catch( (err) => {
+        return res.status(400).json({success: false, error: err})
+    })
+})
 module.exports = router;
